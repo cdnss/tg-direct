9170b80f9dc1e7e3703ddfec0976712f3af50818
cdnss <32522595+cdnss@users.noreply.github.com>
2025-05-10 00:58:43 +0700

    Update proxy.ts

--- main/server/proxy.ts
+++ main/server/proxy.ts
@@ -1,5 +1,5 @@
 // File: proxy.ts
-import * as cheerio from 'https://deno.land/x/cheerio@1.0.0-rc.12/mod.ts';
+import * as cheerio from 'https://esm.sh/cheerio@1.0.0-rc.12';
 import { URL } from 'node:url';
 
 function rewriteUrl(currentPageUrl: string, baseUrl: string, proxyPrefix: string, urlToRewrite: string): string {
