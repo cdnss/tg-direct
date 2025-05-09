c5a254e27224594675ebfbfd2813025c397f608f
cdnss <32522595+cdnss@users.noreply.github.com>
2025-05-10 00:33:10 +0700

    Update __init__.py

--- main/server/__init__.py
+++ main/server/__init__.py
@@ -3,26 +3,28 @@
 # This file is a part of TG-Direct-Link-Generator
 
 from aiohttp import web
-# Import CookieJar di sini karena kita akan membuatnya di sini
 from aiohttp import CookieJar
 from .stream_routes import routes
-from .prox import routes as prox_routes   # Tambahkan baris ini
+# Import routes dan fungsi check dari prox.py
+from .prox import routes as prox_routes, check_deno_and_script
+import asyncio # Perlu import asyncio untuk startup task
+
+# Fungsi yang akan dijalankan saat aplikasi dimulai
+async def startup_tasks(app):
+    # Jalankan pemeriksaan Deno dan script saat aplikasi startup
+    await check_deno_and_script()
 
 def web_server():
     web_app = web.Application(client_max_size=30000000)
 
-    # --- Perbaikan Error: Buat CookieJar DI SINI dan simpan di app ---
-    # Event loop akan segera berjalan setelah web_app siap dijalankan
-    # Ini adalah tempat yang aman untuk membuat CookieJar yang persisten
-    web_app['cookie_jar'] = CookieJar(unsafe=True)
-    # --- End Perbaikan Error ---
+    # Tambahkan tugas startup
+    web_app.on_startup.append(startup_tasks)
 
-    # Tambahkan routes proxy terlebih dahulu (yang berisi /film/...)
+    # Buat CookieJar persisten dan simpan di objek app
+    web_app['cookie_jar'] = CookieJar(unsafe=True)
+
+    # Tambahkan routes
     web_app.add_routes(prox_routes)
-    # Kemudian tambahkan routes streaming/download (yang berisi /{path:\S+})
-    #web_app.add_routes(routes)
+    # web_app.add_routes(routes) # Baris ini tetap dikomentari sesuai input Anda
 
     return web_app
-
-# Pastikan Anda sudah menghapus baris 'app_cookie_jar = CookieJar(unsafe=True)'
-# dari file prox.py seperti yang dibahas sebelumnya.
