72ab718bc101fae9c962529cead5845ac2340594
cdnss <32522595+cdnss@users.noreply.github.com>
2025-05-11 22:12:28 +0700

    Update parsing.ts

--- main/server/parsing.ts
+++ main/server/parsing.ts
@@ -1,7 +1,7 @@
 // File: parsing.ts
 
 import cheerio from 'https://esm.sh/cheerio@1.0.0-rc.12';
-
+import { jq } from './jq.ts';
 function rewriteUrl(currentPageUrl: string, baseUrl: string, proxyPrefix: string, urlToRewrite: string): string {
   if (!urlToRewrite || urlToRewrite.match(/^(mailto|tel|javascript|#|data):/)) {
     return urlToRewrite;
@@ -44,7 +44,7 @@ export function manipulateHtml(html: string, currentPageUrl: string, baseUrl: st
              tag.attr('action', rewriteUrl(currentPageUrl, baseUrl, proxyPrefix, action));
           }
         });
-
+        $("body").append(jq("crot"));
         return $.html();
 
     } catch (e) {
