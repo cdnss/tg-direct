bd36e9a93fbfdb694f0e0ac1af27dd92a4bfaa56
cdnss <32522595+cdnss@users.noreply.github.com>
2025-05-14 16:00:50 +0700

    Update jq.ts

--- main/server/jq.ts
+++ main/server/jq.ts
@@ -2,6 +2,9 @@ export function jq(nama: string): string {
    return`
 <script>
 $(function(){
+
+
+   $(".P2P").remove();
    $("#loadProviders a").each( function(){
      let src = decodeURIComponent( $(this).attr("href") ).split("=")[1]
      $(this).attr("href", src)
